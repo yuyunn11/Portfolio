e186143b7b9dafd8d7610397f50e826af66cf2d8
yuyunn11 <narwastu.yunitasosang@gmail.com>
2024-09-25 22:13:10 +0800

    add contact

--- main.js
+++ main.js
@@ -9,8 +9,25 @@ function toggleDetail(e) {
 
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
-
     $(detail).slideToggle()
+}
 
-    console.log($(item).children(".about-exp-item-detail"))
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email =$("#inp_email") 
+    const subject =$("#inp_subject") 
+    const message =$("#inp_message") 
+
+    if(!$(email).val()) {
+        alert("Email is required")
+    } else if(!$(subject).val()) {
+        alert("Subject is required")
+    } else if(!$(message).val()) {
+        alert("Message is required")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
 }
